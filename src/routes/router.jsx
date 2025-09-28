1a4bf1beccbb53714d6a6dcb792f3a2440b72a9d
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-09-28 13:42:38 +0600

    setup route

---
+++ src/routes/router.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layout/RootLayout";
+import Home from "../Pages/Home/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
